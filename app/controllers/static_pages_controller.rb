f4c997aae9874e0153b94fe9a30f36c357848f4f
joo <syun-315@pc.email.ne.jp>
2017-10-26 05:33:42 +0000

    Add a full_title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
+  
+  helper_method :full_title
+  
   def home
-    @title="home"
   end
 
   def help
@@ -10,4 +12,13 @@ class StaticPagesController < ApplicationController
   def about 
     @title="about"
   end 
+  
+  def full_title(title)
+        base_title = "Ruby on Rails Tutorial"
+        if title 
+            title + '|' + base_title
+        else
+            base_title
+        end
+  end
 end
